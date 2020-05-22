ae4a455e353fb6a52b8c203eed8816ca06c775da
Randolph Roble <roblerm@gmail.com>
2020-05-22 14:12:09 +0800

    Add updater

--- bloomlocal.php
+++ bloomlocal.php
@@ -11,5 +11,21 @@
  * @package Bloomlocal
  */
 
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
 require_once __DIR__ . '/admin_filter_orders_by_delivery_date.php';
 require_once __DIR__ . '/email_format_delivery_phone.php';
+require_once __DIR__ . '/wp_autoupdate.php';
+
+add_action('init', function() {
+    new wp_autoupdate(
+        '0.1.1',
+        'https://arcanys:FvoneOJHEO@bloomlocal.net/dev/update.php',
+        plugin_basename(__FILE__),
+        'rroble',
+        'MIT'
+    );
+});
